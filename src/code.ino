44c025e0e600a7c421c71c86b6baa6b2b7a87a1b
Lucas Myllenno Silva Monteiro Lima <lucas.mlima@hotmail.com.br>
2015-07-26 19:04:05 -0300

    Update

--- src/code.ino
+++ src/code.ino
@@ -1,91 +1,84 @@
-//- Motores: rodas.
-#define motorRight 0
-#define motorLeft 1
-//- Leds: faróis.
-#define lightFrontRight 2
-#define lightFrontLeft 3
-//- Leds: setas.
-#define lightArrowRight 4
-#define lightArrowLeft 5
-//- Leds: freios.
-#define lightBreakRight 6
-#define lightBreakLeft 7
-//- Sensor ultrassônico.
-#define trigUltrasonic 8
-#define echoUltrasonic 9
-//- Botão: liga-desliga.
-#define buttonPower 10
-//- Limite de colisão.
-#define limitCollision 20
+int const motorRight = 2;
+int const motorLeft = 6;
 
-//- Status de funcionamento.
-boolean ligado;
-//- Intervalo de tempo atual.
-int intervalTime;
-//- Direção atual.
-String currentDirection;
-//- Tempo para contagem de segudos.
-int lastTime;
+int const lightFrontRight = 4;
+int const lightFrontLeft = 5;
+int const lightBreakRight = 6;
+int const lightBreakLeft = 7;
+int const lightArrowRight = 8;
+int const lightArrowLeft = 9;
+
+int const trigUltrasonic = 10;
+int const echoUltrasonic = 11;
+
+int const buttonPower = 12;
+
+int const potenciometer = 0;
+
+int limitCollision = 20;
+
+boolean statusActions; //- Status de ações. On / Off
+int intervalTime; //- Tempo definido para uma ação.
+String currentDirection; //- Direção atual.
+int lastTime; //- Auxilia na contagem do tempo.
+int vPot; //- Recebe o valor do potenciometro.
 
 void setup(){
-  	//- Pinos de saída.
-  	for (int i=0; i <= 8; i++){
-  		  pinMode(i, OUTPUT);
-  	}
-  	//- Pinos de entrada.
-  	for (int i=9; i <= 10; i++){
-  		  pinMode(i, INPUT);
-  	}
-    ligado = true;
+    for (int i = 2; i <= 10; i++)
+        pinMode(i, OUTPUT);
+    for (int i = 11; i <= 12; i++)
+        pinMode(i, INPUT);
+    statusActions = true;
     intervalTime = 0;
     currentDirection = "break";
     lastTime = millis();
+    vPot = analogRead(potenciometer) / 4;
 }
 
 void loop(){
-
-    //- liga-desliga.
-  	//power();
-
-    //- Chama processo se estiver ligado.
-    if (ligado == true){
+    vPot = analogRead(potenciometer) / 4;
+    //power(); //- Verifica se está ligado.
+    if (statusActions == true) //- Chama processo se estiver ligado.
         process();
-	  }
+    intervalTime--;
+    delay(1);
 }
 
 void process(){
-    //- Verifica se vai colidir. 
+    //- Verifica se vai colidir.
     int collision = detectCollision();
     if (collision == false){ //- Não vai colidir.
-        if (millis() - lastTime >= intervalTime){
-            lastTime = millis();
+        int timeMillis = millis();
+        if (timeMillis - lastTime >= intervalTime){
+            lastTime = timeMillis;
             motionChange(); //- Muda direção se o tempo esgotou.
-        } else {
-            movement(); //- Mantém direção.
+            movement();
         }
     } else { //- Vai colidir.
-        //- Muda a direção para um dos lados. 
+        //- Muda a direção para um dos lados.
         int generateDirection = random(2);
         if (generateDirection == 0){
             currentDirection = "right";
         } else {
             currentDirection = "left";
         }
-        intervalTime = 1000 * 2;
-        movement(); //- Movimenta o veículo.
-	  }
+        intervalTime = 1000;
+        resetLeds();
+        resetMotors();
+        movement(); //- Movimenta o ve�culo.
+        delay(500);
+    }
 }
 
 void power(){
-  	if (digitalRead(buttonPower) == HIGH){
-        if (ligado == false){
-			      ligado = true;
-    	  } else {
-    		    ligado = false;
-    	  }
+    if (digitalRead(buttonPower) == HIGH){
+        if (statusActions == false)
+            statusActions = true;
+        else
+            statusActions = false;
         intervalTime = 0;
         delay(1000);
-  	}
+    }
 }
 
 boolean detectCollision(){
@@ -95,11 +88,10 @@ boolean detectCollision(){
     delayMicroseconds(5);
     digitalWrite(trigUltrasonic, LOW);
     int distance = pulseIn(echoUltrasonic, HIGH) / 29 / 2;
-    if (distance <= limitCollision){
+    if (distance <= limitCollision)
         return true;
-    } else {
+    else
         return false;
-    }
 }
 
 void motionChange(){
@@ -115,40 +107,40 @@ void motionChange(){
         currentDirection = "break";
     } else {
         intervalTime = 1000;
-        if (generateDirection == 2){
+        if (generateDirection == 2)
             currentDirection = "right";
-        } else {
+        else
             currentDirection = "left";
-        }
     }
     resetLeds();
+    resetMotors();
 }
 
 void movement(){
     if (currentDirection.equals("front")){
-        digitalWrite(motorRight, HIGH);
-        digitalWrite(motorLeft, HIGH);
+        analogWrite(motorRight, vPot);
+        analogWrite(motorLeft, vPot);
         digitalWrite(lightFrontRight, HIGH);
         digitalWrite(lightFrontLeft, HIGH);
     } else if (currentDirection.equals("break")){
-        digitalWrite(motorRight, LOW);
-        digitalWrite(motorLeft, LOW);
         digitalWrite(lightBreakRight, HIGH);
         digitalWrite(lightBreakLeft, HIGH);
     } else if (currentDirection.equals("right")){
-        digitalWrite(motorRight, LOW);
-        digitalWrite(motorLeft, HIGH);
+        analogWrite(motorLeft, vPot);
         digitalWrite(lightArrowRight, HIGH);
     } else if (currentDirection.equals("left")){
-        digitalWrite(motorRight, HIGH);
-        digitalWrite(motorLeft, LOW);
+        analogWrite(motorRight, vPot);
         digitalWrite(lightArrowLeft, HIGH);
     }
 }
 
 void resetLeds(){
-    for (int i=2; i <= 7; i++){
+    for (int i=4; i <= 9; i++)
         digitalWrite(i, LOW);
-    }
+}
+
+void resetMotors(){
+    analogWrite(motorRight, 0);
+    analogWrite(motorLeft, 0);
 }
 
